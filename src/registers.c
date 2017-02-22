3f0984c71ddc59c12ac1391badb616258d583a71
Rhys Thomas <rhys97@me.com>
2017-02-22 00:56:06 +0000

    This is now a source file, rather than just a copy of its header.

--- src/registers.c
+++ src/registers.c
@@ -13,22 +13,7 @@
  * +----------+
  */
 
-#ifndef _REGISTERS_H
-#define _REGISTERS_H
-
-#include <stdint.h>
-#include "global.h"	// access to data and address bus
-
-/**
- * @brief Register structure.
- *
- * Contains program counter, instruction register and accumulator.
- */
-struct {
-	uint16_t pc;	// program counter
-	uint16_t ir;	// instruction register
-	uint16_t ac;	// accumulator
-} regs;
+#include "registers.h"
 
 void read_ac(void)
 {
@@ -40,4 +25,3 @@ void write_ac(void)
 	regs.ac = bus.data;
 }
 
-#endif // _REGISTERS_H
